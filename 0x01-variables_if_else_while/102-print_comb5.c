4e2f7580046523cbc51b74964167ca32f55bd1ad
Oyizaadewuyi <oyizaadewuyi@gmail.com>
2022-03-11 21:24:46 +0000

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,9 +20,7 @@ putchar((num1 % 10) + '0');
 																					putchar((num2 / 10) + '0');
 																					putchar((num2 % 10) + '0');
 
-																					if (num1 == 98 && num2 == 99)
-																					continue;
-
+if (num1 == 98 && num2 == 99)																		continue
 																					putchar(',');
 																					putchar(' ');
 																					}
