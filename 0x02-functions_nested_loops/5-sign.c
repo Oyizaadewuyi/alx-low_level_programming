b10249d71d308a11a6af126b9f7b8fc3193cf785
Oyizaadewuyi <oyizaadewuyi@gmail.com>
2022-03-15 20:22:46 +0000

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,15 +14,13 @@ if (n > 0)
 {
 _putchar('+');
 return (1);
-}
-else if (n == 0)
+} else if (n == 0)
 {
-_putchar('0');
+_putchar(48);
 return (0);
-}
-else
+ else if (n < 0)
 {
-_putchar('-');
+																					_putchar('-');
+																					}
 return (-1);
-}
 }
